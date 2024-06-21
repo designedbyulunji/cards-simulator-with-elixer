e7a9d964cfb0cd4fe31d06b70f92e0b5eb4a1bc2
Ulunji Felix Mkandawire <designedbyulunji@gmail.com>
2024-06-21 17:49:43 +0200

    Defined a method to create a new deck of cards

--- lib/cards_simulator.ex
+++ lib/cards_simulator.ex
@@ -1,3 +1,13 @@
 defmodule CardsSimulator do
+  # Method that creates a deck of cards
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suites = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suite <- suites do
+      "#{value} of #{suite}"
+    end
+
+  end
 
 end
