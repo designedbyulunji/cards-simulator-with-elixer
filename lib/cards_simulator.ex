d29f4764c1949b2f32096185d189fca1105d07cf
Ulunji Felix Mkandawire <designedbyulunji@gmail.com>
2024-06-21 18:10:33 +0200

    Defined methods for saving and loading files

--- lib/cards_simulator.ex
+++ lib/cards_simulator.ex
@@ -19,4 +19,24 @@ defmodule CardsSimulator do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  # Method to create a hand
+  def create_hand(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  # Method to save file to local storage
+  def save_file(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  # Method to load file
+  def load_file(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File does not exist"
+
+    end
+  end
 end
